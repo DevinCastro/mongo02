c5c9f20b7514cecaa2a5d29a5d3f0efa31ec67d6
Devin Castro <dcrocker21@gmail.com>
2020-09-10 13:27:23 -0700

    register and login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,31 +1,55 @@
 const router = require('express').Router()
 const { User } = require('../models')
+const jwt = require('jsonwebtoken')
 
-router.get('/users', (req, res) => {
-  // leave empty to get back everything
-  User.find()
-    .populate('items')
-    .then(item => res.json(item))
-    .catch(err => console.log(err))
+// register with passport
+router.post('/users/register', (req,res) => {
+  const { name, email, username, password } = req.body
+  User.register(new User({ name, email, username }), password, err => {
+    if (err) {console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+// login route
+router.post('/users/login', (req,res) => {
+  const { usernamem, password } = req.body
+  User.authenticate()(username, password, (err, user) => {
+    if (err) {console.log(err)}
+    res.json(user ? jwt.sign({ id: user._id }, process.env.SECRET) : null)
+  })
 })
 
 
-router.post('/users', (req, res) => {
-  User.create(req.body)
-    .then(item => res.json(item))
-    .catch(err => console.log(err))
-})
 
-router.put('/users/:id', (req, res) => {
-  User.findByIdAndUpdate(req.params.id, req.body)
-    .then(() => res.sendStatus(200))
-    .catch(err => console.log(err))
-})
 
-router.delete('/users/:id', (req, res) => {
-  User.findByIdAndDelete(req.params.id)
-    .then(() => res.sendStatus(200))
-    .catch(err => console.log(err))
-})
+
+
+// router.get('/users', (req, res) => {
+//   // leave empty to get back everything
+//   User.find()
+//     .populate('items')
+//     .then(item => res.json(item))
+//     .catch(err => console.log(err))
+// })
+
+
+// router.post('/users', (req, res) => {
+//   User.create(req.body)
+//     .then(item => res.json(item))
+//     .catch(err => console.log(err))
+// })
+
+// router.put('/users/:id', (req, res) => {
+//   User.findByIdAndUpdate(req.params.id, req.body)
+//     .then(() => res.sendStatus(200))
+//     .catch(err => console.log(err))
+// })
+
+// router.delete('/users/:id', (req, res) => {
+//   User.findByIdAndDelete(req.params.id)
+//     .then(() => res.sendStatus(200))
+//     .catch(err => console.log(err))
+// })
 
 module.exports = router
